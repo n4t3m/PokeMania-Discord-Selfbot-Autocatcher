af33990aec2e62f241c0d5dcb73f77f5685c6271
NateM135 <melwaninate@gmail.com>
2020-06-27 09:34:56 -0700

    Added new varibales for choice of pokeball/amount to buy when out

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 #Enter Your bot token  and prefix here inbetween the quotes.
 #Capture Chance: 100 = 100%, 90 = 90%, etc
 
-PREFIX = ';'
+PREFIX = '-'
 TOK = ""
 DELAY = 2
 OWNER_UID = 0
 SELFBOT_UID = 0
+
+#Capture Chance: 100 = 100%, 90 = 90%, etc
 CAPTURE_CHANCE = 100
+
+#BALL_TYPE: Acceptable values are POKEBALL, GREATBALL, ULTRABALL
+BALL_TYPE = "ULTRABALL"
+
+#BUY_BALL_AMOUNT - When out of Pokeballs this will be the default amount purcahsed.
+BUY_BALL_AMOUNT = 5
